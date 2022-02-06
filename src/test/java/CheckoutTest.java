e0708c02789673758c3e45bd44ed08589207257b
Yuriy_Nikitchenko <yuriy_nikitchenko@epam.com>
2022-02-06 17:30:16 +0200

    HW refactored (cleaned)

--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,18 +9,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class CheckoutTest {
+class CheckoutTest {
 
     public static final String DRIVER_PATH = "src/drivers/chromedriver.exe";
-    public static final String THE_URL = "https://www.bookdepository.com/";
-    public static final String THE_BASKET_URL = "https://www.bookdepository.com/basket";
-    public static final String THE_CHECKOUT_URL = "https://www.bookdepository.com/payment/guest";
+    public static final String THE_URL = "https://www.bookdepository.com";
+    public static final String THE_BASKET_URL = THE_URL + "/basket";
+    public static final String THE_CHECKOUT_URL = THE_URL + "/payment/guest";
 
     public static final String THE_BOOK_NAME = "Selenium Design Patterns and Best Practices";
     public static final String THE_BOOK_ISBN13 = "9781783982707";
@@ -31,7 +30,13 @@ public class CheckoutTest {
     // XPATHes:
     public static final String SEARCH_FIELD_XPATH = "//input[@name ='searchTerm']";
     public static final String BASKET_PRODUCT_COUNTER_XPATH = "//div[@class = 'right-section']//a[@href = '/basket']/span[@class = 'item-count']";
+    public static final String LINK_TO_THE_BOOK_FROM_SEARCH_RESULT_XPATH = "//meta[@content = '9781783982707']/..//h3//a[contains(text(),'Selenium Design Patterns and Best Practices')]";
+
+    public static final String PDP_H1_TEXT_XPATH = "//h1[text() = 'Selenium Design Patterns and Best Practices']";
+    public static final String PDP_ISBN13_XPATH = "//label[text() = 'ISBN13']/following-sibling::span[text() = '9781783982707']";
+    public static final String ADD_TO_BASKET_FROM_PDP_XPATH = "//a[@class = 'btn btn-primary add-to-basket' and @data-isbn = '9781783982707'  and contains(text(),'Add to basket')]";
     public static final String ITEM_ADDED_MESSAGE_XPATH = "//h3[@class = 'modal-title' and text() = 'Item added to your basket']";
+    public static final String TO_BASKET_FROM_POPUP_XPATH = "//a[@href = '/basket'  and contains(text(),'Basket / Checkout')]";
 
     public static final String BASKET_ITEM_TOTAL_XPATH = "//p[@class = 'item-total']";
     public static final String BASKET_TOTAL_XPATH = "//dl[@class = 'total']/dd";
@@ -45,132 +50,117 @@ public class CheckoutTest {
     public static final String CHECKOUT_EMAIL_ADDRESS_FIELD_ERROR_XPATH = "//input[@name='emailAddress']/following-sibling::div[@class = 'error-block']";
     public static final String CHECKOUT_BUY_NOW_BTN_XPATH = "//button[text() = 'Buy now']";
 
-
-    static WebDriver driver;
+    private static WebDriver driver;
 
     @BeforeAll
     static void driverSetup() {
         System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
-        System.out.println("Before All");
     }
 
-     void searchTheBook(WebDriver driver) {
-        WebElement search = locateByXpath(driver, SEARCH_FIELD_XPATH);
-        //search.clear();
+    @AfterAll
+    static void driverQuit() {
+        driver.quit();
+        driver = null;
+    }
+
+    void searchTheBook() {
+        WebElement search = locateByXpath(SEARCH_FIELD_XPATH);
         search.sendKeys(THE_BOOK_NAME);
         search.sendKeys(Keys.ENTER);
     }
 
-     WebElement locateByXpath(WebDriver driver, String xPath) {
-        WebElement webElement = driver.findElement(By.xpath(xPath));
-        return webElement;
+    WebElement locateByXpath(String xPath) {
+        return driver.findElement(By.xpath(xPath));
     }
 
     @Test
-    public void testCheckoutFlow() {
+    void testCheckoutFlow() {
 
-        // Set Page
         driver.get(THE_URL);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
+        System.out.println("Start the process of adding the Book to the Basket.");
 
-        System.out.println("\nStart theProcess of adding the Book to the Basket");
+        searchTheBook();
 
-        searchTheBook(driver);
-
-        WebElement linkToTheBook = driver.findElement(By
-                .xpath("//meta[@content = '9781783982707']" +
-                        "/..//h3//a[contains(text(),'Selenium Design Patterns and Best Practices')]"));
+        WebElement linkToTheBook = locateByXpath(LINK_TO_THE_BOOK_FROM_SEARCH_RESULT_XPATH);
         linkToTheBook.click();
 
-        boolean isTheNeededPDP;
-        isTheNeededPDP = driver.getCurrentUrl().contains(THE_BOOK_ISBN13);
-        //Assert
-        System.out.println("Is the needed pdp? " + isTheNeededPDP);
+        WebElement h1PDPText = locateByXpath(PDP_H1_TEXT_XPATH);
+        WebElement isbn13 = locateByXpath(PDP_ISBN13_XPATH);
+        WebElement addToBasketBtn = locateByXpath(ADD_TO_BASKET_FROM_PDP_XPATH);
 
-        WebElement addToBasket = driver.findElement(By
-                .xpath("//a[@class = 'btn btn-primary add-to-basket' and @data-isbn = '9781783982707'  and contains(text(),'Add to basket')]"));
-        addToBasket.click();
+        assertAll("Check the PDP",
+                () -> assertTrue(driver.getCurrentUrl().contains(THE_BOOK_ISBN13),
+                        "Not expected PDP."),
+                () -> assertEquals(THE_BOOK_ISBN13, isbn13.getText(),
+                        "Not expected ISBN13."),
+                () -> assertEquals(THE_BOOK_NAME, h1PDPText.getText(),
+                        "Not expected the Book Name on PDP."),
+                () -> assertNotNull(addToBasketBtn,
+                        "Add to the Basket Button not exist (probably the the Book is OOS).")
+        );
+
+        addToBasketBtn.click();
+
 
         WebDriverWait wait = new WebDriverWait(driver, 15);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(ITEM_ADDED_MESSAGE_XPATH)));
-        WebElement itemAddedMessage = locateByXpath(driver, ITEM_ADDED_MESSAGE_XPATH);
+        WebElement itemAddedMessage = locateByXpath(ITEM_ADDED_MESSAGE_XPATH);
         System.out.println(itemAddedMessage.getText());
 
-        WebElement toTheBasketBtn = driver.findElement(By.xpath("//a[@href = '/basket'  and contains(text(),'Basket / Checkout')]"));
+        WebElement toTheBasketBtn = locateByXpath(TO_BASKET_FROM_POPUP_XPATH);
         toTheBasketBtn.click();
 
-        System.out.println(driver.getCurrentUrl());
 
-        // Check2
-        WebElement basketProductCounterAfterAddingTheBook = locateByXpath(driver, BASKET_PRODUCT_COUNTER_XPATH);
-        System.out.println("Products in the Basket after: " + basketProductCounterAfterAddingTheBook.getText());
-        int expectedQuantityOfProductsInTheBasketAfter = 1;
+        WebElement basketItemTotal = locateByXpath(BASKET_ITEM_TOTAL_XPATH);
+        WebElement basketTotal = locateByXpath(BASKET_TOTAL_XPATH);
+        WebElement basketProductCounterAfterAddingTheBook = locateByXpath(BASKET_PRODUCT_COUNTER_XPATH);
+        int expectedQuantityOfProductsInTheBasketAfterAdding = 1;
 
-        // end Check2
-
-
-        WebElement basketItemTotal = locateByXpath(driver, BASKET_ITEM_TOTAL_XPATH);
-        System.out.println(basketItemTotal.getText());
-        WebElement basketTotal = locateByXpath(driver, BASKET_TOTAL_XPATH);
-        System.out.println(basketTotal.getText());
-
-        assertAll("Check The Basket",
-                () -> assertEquals(THE_BASKET_URL , driver.getCurrentUrl(),
+        assertAll("Check the Basket",
+                () -> assertEquals(THE_BASKET_URL, driver.getCurrentUrl(),
                         "Not in the Basket."),
-                () -> assertTrue(expectedQuantityOfProductsInTheBasketAfter == Integer.valueOf(basketProductCounterAfterAddingTheBook.getText().trim()),
+                () -> assertEquals(expectedQuantityOfProductsInTheBasketAfterAdding,
+                                Integer.parseInt(basketProductCounterAfterAddingTheBook.getText().trim()),
                         "Not expected quantity of the products in the Mini Basket After the Book added."),
-                () -> assertEquals(THE_BOOK_PRICE , basketItemTotal.getText(),
+                () -> assertEquals(THE_BOOK_PRICE, basketItemTotal.getText(),
                         "Not correct Subtotal in the Basket."),
-                () -> assertEquals(THE_BOOK_PRICE , basketTotal.getText(),
+                () -> assertEquals(THE_BOOK_PRICE, basketTotal.getText(),
                         "Not correct Subtotal in the Basket.")
         );
 
-        WebElement basketStartCheckoutBtn = locateByXpath(driver, BASKET_CHECKOUT_BTN_XPATH );
+        WebElement basketStartCheckoutBtn = locateByXpath(BASKET_CHECKOUT_BTN_XPATH);
         basketStartCheckoutBtn.click();
 
 
-        WebElement checkoutSubtotal = locateByXpath(driver, CHECKOUT_SUBTOTAL_XPATH);
-        System.out.println(checkoutSubtotal.getText());
-        WebElement checkoutTotal = locateByXpath(driver, CHECKOUT_TOTAL_XPATH);
-        System.out.println(checkoutTotal.getText());
-        WebElement checkoutVAT = locateByXpath(driver, CHECKOUT_VAT_XPATH);
-        System.out.println(checkoutVAT.getText());
+        WebElement checkoutSubtotal = locateByXpath(CHECKOUT_SUBTOTAL_XPATH);
+        WebElement checkoutTotal = locateByXpath(CHECKOUT_TOTAL_XPATH);
+        WebElement checkoutVAT = locateByXpath(CHECKOUT_VAT_XPATH);
 
-        WebElement emailAddress = locateByXpath(driver, CHECKOUT_EMAIL_ADDRESS_FIELD_XPATH);
+        WebElement emailAddress = locateByXpath(CHECKOUT_EMAIL_ADDRESS_FIELD_XPATH);
         emailAddress.sendKeys(THE_EMAIL_ADDRESS);
-
-        WebElement buyNowBtn = locateByXpath(driver, CHECKOUT_BUY_NOW_BTN_XPATH);
+        WebElement buyNowBtn = locateByXpath(CHECKOUT_BUY_NOW_BTN_XPATH);
         buyNowBtn.click();
+        WebElement emailError = locateByXpath(CHECKOUT_EMAIL_ADDRESS_FIELD_ERROR_XPATH);
 
-        WebElement emailError = locateByXpath(driver, CHECKOUT_EMAIL_ADDRESS_FIELD_ERROR_XPATH);
-        System.out.println(emailError.getText());
-
-
-        assertAll("Check The Checkout",
-                () -> assertEquals(THE_CHECKOUT_URL , driver.getCurrentUrl(),
+        assertAll("Check the Checkout Page",
+                () -> assertEquals(THE_CHECKOUT_URL, driver.getCurrentUrl(),
                         "Not in the Checkout page."),
-                () -> assertEquals(THE_BOOK_PRICE , checkoutSubtotal.getText(),
+                () -> assertEquals(THE_BOOK_PRICE, checkoutSubtotal.getText(),
                         "Not correct Subtotal in the Checkout page."),
-                () -> assertEquals(THE_BOOK_VAT , checkoutVAT.getText(),
+                () -> assertEquals(THE_BOOK_VAT, checkoutVAT.getText(),
                         "Not correct VAT in the Checkout page."),
-                () -> assertEquals(THE_BOOK_PRICE , checkoutTotal.getText(),
+                () -> assertEquals(THE_BOOK_PRICE, checkoutTotal.getText(),
                         "Not correct Total in the Checkout page."),
-                () -> assertEquals("" , emailError.getText(),
+                () -> assertEquals("", emailError.getText(),
                         "Email error appeared.")
         );
 
-    }
-
-    @AfterAll
-    static void driverQuit() throws InterruptedException {
-        Thread.sleep(3000);
-        driver.quit();
-        driver = null;
-        System.out.println("After All");
+        System.out.println("The process is finished.");
     }
 
 }
